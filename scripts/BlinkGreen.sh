35aa7ad1ddb11817cf08680d0ebc6906d232adef
Hajime MIZUNO <h-mizuno@infiniteloop.co.jp>
2016-12-26 11:04:46 +0900

    drop sample script

--- scripts/BlinkGreen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/curl -s "http://localhost/ctrl/?color=0,100,0&mode=1" > /dev/null
-
--- scripts/BlinkRed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/curl -s "http://localhost/ctrl/?color=100,0,0&mode=1" > /dev/null
-
-
